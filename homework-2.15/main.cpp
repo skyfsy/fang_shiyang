7a7a3de5db9387c43948b254ac729a1ea31b9752
Your Name <25019675662qq.com>
2019-03-11 18:35:10 +0800

    更改

--- homework-2.15/main.cpp
+++ homework-2.15/main.cpp
@@ -15,34 +15,34 @@ int main()
 
    add=number_1+number_2;
    cut=number_1-number_2;
-   ride=number_1/number_2;
+   ride=number_1*number_2;
    except=number_1*number_2;
 
-   cout<<"the add is :"<<add<<endl;
-   cout<<"the ride is :"<<ride<<endl;
+   cout<<"add= "<<add<<endl;
+   cout<<"ride= "<<ride<<endl;
    cout<<endl;
 
    cout<<"if from left to right :"<<endl;
-   cout<<"cut is :"<<cut<<endl;
+   cout<<"cut= "<<cut<<endl;
    if(0==number_2)
    {
-       cout<<"result of except is meaningless"<<endl;
+       cout<<"^v^! ^v^! result of except is meaningless"<<endl;
    }
    else
    {
-       cout<<"except is :"<<except<<endl;
+       cout<<"except= "<<except<<endl;
    }
    cout<<endl;
 
    cout<<"if from right to left :"<<endl;
-   cout<<"the cut is :"<<-cut<<endl;
+   cout<<"cut= "<<-cut<<endl;
    if(0==number_1)
    {
-       cout<<"result of except is meaningless"<<endl;
+       cout<<"^v^! ^v^! result of except is meaningless"<<endl;
    }
    else
    {
-       cout<<"except is :"<<number_2/number_1<<endl;
+       cout<<"except= "<<number_2/number_1<<endl;
    }
 
 }
