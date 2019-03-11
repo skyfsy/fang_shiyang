082ccb4403920510947933768330441fcd0ac3d8
Your Name <25019675662qq.com>
2019-03-11 16:37:42 +0800

    ok

--- homework-2.15/main.cpp
+++ homework-2.15/main.cpp
@@ -4,6 +4,45 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+   double number_1,number_2;
+   double add=0;
+   double cut=0;
+   double ride=0;
+   double except=0;
+
+   cout<<"please put two number that you want to calculate :"<<endl;
+   cin>>number_1>>number_2;
+
+   add=number_1+number_2;
+   cut=number_1-number_2;
+   ride=number_1/number_2;
+   except=number_1*number_2;
+
+   cout<<"the add is :"<<add<<endl;
+   cout<<"the ride is :"<<ride<<endl;
+   cout<<endl;
+
+   cout<<"if from left to right :"<<endl;
+   cout<<"cut is :"<<cut<<endl;
+   if(0==number_2)
+   {
+       cout<<"result of except is meaningless"<<endl;
+   }
+   else
+   {
+       cout<<"except is :"<<except<<endl;
+   }
+   cout<<endl;
+
+   cout<<"if from right to left :"<<endl;
+   cout<<"the cut is :"<<-cut<<endl;
+   if(0==number_1)
+   {
+       cout<<"result of except is meaningless"<<endl;
+   }
+   else
+   {
+       cout<<"except is :"<<number_2/number_1<<endl;
+   }
+
 }
