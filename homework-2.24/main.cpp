04afbbf91a6d6918626a7c34055216d47cedc3a3
Your Name <25019675662qq.com>
2019-03-12 17:18:43 +0800

    2.24

---
+++ homework-2.24/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"please input the number : ";
+    cin>>number;
+    if(0==number%2)
+    {
+        cout<<number<<" is Å¼Êı"<<endl;
+    }
+    else
+    {
+        cout<<number<<" is ÆæÊı"<<endl;
+    }
+    return 0;
+}
